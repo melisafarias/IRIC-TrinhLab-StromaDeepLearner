9a5d04b74123fbd964a90500e5a00f91995990dd
melisafarias <133890196+melisafarias@users.noreply.github.com>
2023-09-27 00:34:16 -0400

    Add files via upload

---
+++ Renaming_Tiles.py
@@ -0,0 +1,10 @@
+import os
+
+import os
+
+# Absolute path of a file
+old_name = r"E:\demos\files\reports\details.txt"
+new_name = r"E:\demos\files\reports\new_details.txt"
+
+# Renaming the file
+os.rename(old_name, new_name)
